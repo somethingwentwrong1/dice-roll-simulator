8ade750b8b72e60695251bcbd08496641b19e2c6
[Jonny Choi] <jonnychoi@seoulchristian.org>
2016-01-22 17:10:00 +0900

    Add dice-roll-sim to dice-roll repo

---
+++ dice-roll-simulator.py
@@ -0,0 +1,6 @@
+#Generates random string.
+import random
+#Set dice as random.randint which returns random integers from low to high.
+dice = random.randint(1, 6)
+#Print dice num = the random number from 1 to 6.
+print "dice num =", dice
